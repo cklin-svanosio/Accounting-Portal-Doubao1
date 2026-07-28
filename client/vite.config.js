f93fd24b03a207e7a1a9a1cc67bb08a2b186bd84
cklin-svanosio <sk.lin@svanosio.com>
2026-07-28 16:20:50 +0800

    vite設定排除api目錄，嘗試跳過私有套件TS錯誤

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,4 +4,10 @@ import vue from '@vitejs/plugin-vue'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  build: {
+    rollupOptions: {
+      // 忽略 api 相關檔案（遠端殘留的 api/ 不會被打包讀取）
+      external: file => file.includes('/api/')
+    }
+  }
 })
